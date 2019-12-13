02ae29dfcac9501153ae3730e707e55592dfaef1
iding-ir <aydin.ghane.kh@gmail.com>
2019-12-13 08:28:17 +0330

    Added css files and created plain layouts

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,33 @@ import "./app.css";
 class App extends Component {
   state = {};
   render() {
-    return <div></div>;
+    return (
+      <div className="layout-container">
+        <div className="layout-xl">
+          <div className="layout-content">
+            <span>LG</span>
+          </div>
+        </div>
+
+        <div className="layout-l">
+          <div className="layout-content">
+            <span>L</span>
+          </div>
+        </div>
+
+        <div className="layout-sm">
+          <div className="layout-content">
+            <span>SM</span>
+          </div>
+        </div>
+
+        <div className="layout-sm">
+          <div className="layout-content">
+            <span>SM</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./css/reset.css";
+import "./css/fonts.css";
+import "./css/index.css";
 import App from "./components/app";
 
 import * as serviceWorker from "./serviceWorker";
